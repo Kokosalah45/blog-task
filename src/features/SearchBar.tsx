367e4306ffbd0333fbe09bb5a7184d8489a38963
Kerolous Amged <kerolous.amged@nagwa.com>
2024-02-24 09:00:54 +0200

    refactor: modifications to searchParams

--- src/features/SearchBar.tsx
+++ src/features/SearchBar.tsx
@@ -1,38 +1,39 @@
-import { useSearchParams } from '@/Providers/SearchParamsProvider';
-import { Search } from 'lucide-react'
+import { useSearchParams } from "@/Providers/SearchParamsProvider";
+import { Search } from "lucide-react";
 
 const SearchBar = () => {
+  const { getSearchParam, setSearchParam } = useSearchParams();
 
-    const { getSearchParam , setSearchParam } = useSearchParams();
-    
-    const searchParam = getSearchParam('search') ?? ""
-    console.log({searchParam})
+  const searchParam = getSearchParam("search") ?? "";
+
   return (
     <form
-    onSubmit={e => {
+      onSubmit={(e) => {
         e.preventDefault();
-        setSearchParam('search' , e.target['search'].value);
-    }}
-    className=" border relative rounded-xl overflow-hidden mb-5 "
-  >
-    <span
-      className="opacity-100 rounded-s-xl px-2 h-full absolute  flex items-center mb-1 "
+        const formData = new FormData(e.currentTarget);
+        setSearchParam("search", formData.get("search") as string);
+      }}
+      className=" border relative rounded-xl overflow-hidden mb-5 "
     >
-      <Search color="gray" />
-    </span>
-   <div className='flex'>
-   <input
-      className=" ps-12 outline-none p-2 rounded-xl w-full"
-      type="text"
-      name="search"
-      defaultValue={searchParam}
-    />
-    <button type='submit' className='bg-slate-500 px-11 hover:bg-slate-700 transition-all text-white text-lg'>
-        search
-    </button>
-   </div>
-  </form>
-  )
-}
+      <span className="opacity-100 rounded-s-xl px-2 h-full absolute  flex items-center mb-1 ">
+        <Search color="gray" />
+      </span>
+      <div className="flex">
+        <input
+          className=" ps-12 outline-none p-2 rounded-xl w-full"
+          type="text"
+          name="search"
+          defaultValue={searchParam}
+        />
+        <button
+          type="submit"
+          className="bg-slate-500 px-11 hover:bg-slate-700 transition-all text-white text-lg"
+        >
+          search
+        </button>
+      </div>
+    </form>
+  );
+};
 
-export default SearchBar
+export default SearchBar;
